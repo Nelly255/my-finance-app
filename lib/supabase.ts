4e75478fdbab88a524f7d727068f7f595189a415
Nelly Jackson <nellyjackson81@gmail.com>
2026-01-12 22:36:27 +0300

    Restore original supabase config

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Replace these strings with your actual values from Supabase
-const supabaseUrl = 'https://ytilwerkyezrzwjskeut.supabase.co' 
-const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0aWx3ZXJreWV6cnp3anNrZXV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgxNTQzMDgsImV4cCI6MjA4MzczMDMwOH0.PrS3n5bDo2bsCGg5thsCpRNE-5GlCE7rGIlEtmoLe20
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
